6c4f955e80f210148101e1e72e4192cc5f1427b7
philballer <philballer41@gmail.com>
2023-08-31 22:48:51 +0100

    Converting strings in files to caps

--- working_with_files/accessing_files.py
+++ working_with_files/accessing_files.py
@@ -1,2 +1,13 @@
-opening = open('sample.txt')
-print(opening)
+opening = open('mbox.txt', 'r')
+new_file = open('everything.txt', 'w')
+'''
+for all in opening:
+    if all.startswith('From:'):
+        new_file.write(all)
+'''
+count = 0
+for all in opening:
+    if all.startswith('Subject'):
+        count = count + 1
+
+print('There are', count, 'words called complete')
---
+++ working_with_files/text_caps.py
@@ -0,0 +1,11 @@
+short_text = open('mbox.txt', 'r')
+reading_text = short_text.read()
+final = reading_text.upper()
+
+# for v in final:
+print(final)
+
+
+
+
+    
