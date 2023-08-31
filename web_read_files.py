358304782eb055138e95025d9c57a79ffe139aab
philballer <philballer41@gmail.com>
2023-08-31 02:56:49 +0100

    Printing all data downloaded from web scrapping script

--- web_read_files.py
+++ web_read_files.py
@@ -1,10 +1,13 @@
 from urllib import request
 import random
 
-file_url = ""
+file_url = "https://www.google.com"
 
 def online_data(new_data):
     getting = request.urlopen(new_data)
     downloading = getting.read()
     download_str = str(downloading)
     lines = download_str.split("\\n")
+    print(lines)
+
+online_data(file_url)
