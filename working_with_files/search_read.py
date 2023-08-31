6bd1c5582f8b911267f9f1acb7e678ac9863fa4c
philballer <philballer41@gmail.com>
2023-09-01 00:04:43 +0100

    Computing values from files

---
+++ working_with_files/search_read.py
@@ -0,0 +1,25 @@
+def finding_confidence(file_string):
+    begin = file_string.find(':')
+    stop = file_string.find('\'', begin)
+    complete = file_string[begin + 1:stop]
+    conversion = float(complete)
+    return conversion
+
+file_name = input('Enter the name of the file: ')
+try:
+    open_file = open(file_name)
+except:
+    print('Directory not found!! Check name and try again')
+    exit()
+
+confidence = 'X-DSPAM-Confidence: 0.8475'
+count = 0
+total = 0.0
+for v in open_file:
+    if v.startswith('X-DSPAM-Confidence: '):
+        count = count + 1
+        confidence_found = finding_confidence(v)
+        total = total + confidence_found
+
+average = total/count
+print('Average spam confidence: ', average)
