0a3e5523909aae220efd4549122cf2a50bf7a480
philballer <philballer41@gmail.com>
2023-09-01 17:48:39 +0100

    Final edits

--- lists/finding_unique_words_in_file.py
+++ lists/finding_unique_words_in_file.py
@@ -1,15 +1,16 @@
-romeo = open('romeo.txt')
-
-unique_words = list()
+romeo = open('romeo.txt', 'r')
+decoding = romeo.read()  
+general = decoding.strip()
+unique_words = []
 count = 0
 
-for v in romeo:
-    main_words = v.rstrip()
-    if v in main_words:
-        all_words = v.split()
-        print(all_words)
+for v in general:
+    all_words = v.split()
+    if all_words not in unique_words:
+        unique_words.append(all_words)  
     count += 1
 
+print(unique_words)
 print(count)
 '''unique_words.sort()
 for x in unique_words:
