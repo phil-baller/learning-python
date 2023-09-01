51bd4ebb66509adfad01ac5e2e9a4e9f01111586
philballer <philballer41@gmail.com>
2023-09-01 23:01:02 +0100

    corrected program for finding unique word

--- lists/finding_unique_words_in_file.py
+++ lists/finding_unique_words_in_file.py
@@ -1,18 +1,19 @@
-romeo = open('romeo.txt', 'r')
-decoding = romeo.read()  
-general = decoding.strip()
+romeo = open('romeo.txt')
+    
+def decoding(reading, words):
+    delimiter = ' '
+    count = 0
+    for v in reading:
+        decoding = v.split(delimiter)
+        for x in decoding:
+            if x not in words:
+                words.append(x)
+                count += 1
+    words.sort()
+    print(words)
+    print(count)
+
+
 unique_words = []
-count = 0
 
-for v in general:
-    all_words = v.split()
-    if all_words not in unique_words:
-        unique_words.append(all_words)  
-    count += 1
-
-print(unique_words)
-print(count)
-'''unique_words.sort()
-for x in unique_words:
-    print(x)    
-'''
+decoding(romeo, unique_words)
