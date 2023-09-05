835e541772b0f4e304a0b9820460d2cf292d36e3
philballer <philballer41@gmail.com>
2023-09-05 12:54:31 +0100

    getting unique words in string of words using re

--- regular expressions/specific_expressions.py
+++ regular expressions/specific_expressions.py
@@ -1,9 +1,15 @@
 import re
 
-data = open('mbox.txt')
-count = {}
-for lines in data:
+data = open('mbox-short.txt')
+
+"""for lines in data:
     lines = lines.rstrip()
     x = re.findall('[a-zA-Z0-9]\S+@\S+[a-zA-Z]', lines)
+    if len(x) > 0:
+        print(x)"""
+
+for lines in data:
+    lines = lines.rstrip()
+    x = re.findall('^X\S+: ([0-9.]+)', lines)
     if len(x) > 0:
         print(x)
