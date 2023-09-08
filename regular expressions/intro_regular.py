2e904a469fad73e168477c7bf0119c42e0f2be85
phil-baller <philballer41@gmail.com>
2023-09-08 02:44:11 -0700

    Learning basics of classes

--- regular expressions/intro_regular.py
+++ regular expressions/intro_regular.py
@@ -1,6 +1,6 @@
 import re
 
-fhand = open('mbox-short.txt')
+fhand = open('../mbox-short.txt')
 for v in fhand:
     v = v.rstrip()
     #if re.search('F..m:', v):
--- regular expressions/specific_expressions.py
+++ regular expressions/specific_expressions.py
@@ -1,15 +1,19 @@
 import re
+import sys
 
 data = open('mbox-short.txt')
 
-"""for lines in data:
+for lines in data:
     lines = lines.rstrip()
     x = re.findall('[a-zA-Z0-9]\S+@\S+[a-zA-Z]', lines)
     if len(x) > 0:
+        print(x)
+
+    """if len(x) > 0:
         print(x)"""
 
-for lines in data:
+"""for lines in data:
     lines = lines.rstrip()
     x = re.findall('^X\S+: [0-9.]+', lines)
     if len(x) > 0:
-        print(x)
+        print(x)"""
